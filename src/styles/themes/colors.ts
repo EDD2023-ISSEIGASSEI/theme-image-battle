837e0449f777fd08e6a2dd431f0c35a924742641
uuronn <uuronprogram@gmail.com>
2023-09-05 22:11:52 +0900

    Add: カラーを追加

---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  primary: "var(--primary)",
+
+  blue: "var(--blue)",
+  red: "var(--red)",
+  green: "var(--green)",
+  black: {
+    primary: "var(--black)",
+    lighten: {
+      1: "var(--black-lighten-1)",
+      2: "var(--black-lighten-2)",
+      3: "var(--black-lighten-3)",
+      4: "var(--black-lighten-4)",
+      5: "var(--black-lighten-5)"
+    }
+  },
+  white: "var(--white)"
+};
