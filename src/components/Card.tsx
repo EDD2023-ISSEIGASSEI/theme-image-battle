7c2933079db28dc3facbc378244fd42c8c0c3a58
uuronn <uuronprogram@gmail.com>
2023-09-05 22:12:08 +0900

    Create: Cardコンポーネントの実装

---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+import { css } from "@emotion/react";
+import { ReactNode } from "react";
+import { colors } from "../styles/themes/colors";
+
+type CardProps = {
+  children: ReactNode;
+};
+
+export const Card = ({ children }: CardProps) => {
+  return <div css={card}>{children}</div>;
+};
+
+const card = css`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 40px 116px;
+  border-radius: 20px;
+  background: ${colors.primary};
+  box-shadow: 8px 8px 16px #af9e43, -8px -8px 16px #ffff73;
+`;
