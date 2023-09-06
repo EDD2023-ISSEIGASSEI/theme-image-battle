f35867c67dc31c77e524747b14cb94477aca3d27
uuronn <uuronprogram@gmail.com>
2023-09-06 18:22:29 +0900

    Fix: type-moduleがなかったかも

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div>
-      {/* @ts-ignore */}
       hello world
       <a href="sample">いいい</a>
       <Link to="sample">ああああ</Link>
