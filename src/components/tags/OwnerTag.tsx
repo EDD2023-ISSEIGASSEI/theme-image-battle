90b2aa8f561f9b6bccd28a534912070f5d74b385
uuronn <uuronprogram@gmail.com>
2023-09-07 13:28:54 +0900

    Create: オーナータグコンポーネントを作成

---
+++ src/components/tags/OwnerTag.tsx
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+import { colors } from "~/styles/themes/colors";
+
+export const OwnerTag = () => {
+  return <span css={tag}>オーナー</span>;
+};
+
+const tag = css`
+  width: 80px;
+  height: 28px;
+  padding: 0 8px;
+  border-radius: 8px;
+  text-align: center;
+  font-size: 1.4rem;
+  line-height: 28px;
+  box-shadow: 2px 2px 4px #af9e43, -2px -2px 4px #ffff73;
+  color: ${colors.black.lighten[1]};
+  background: ${colors.red};
+`;
