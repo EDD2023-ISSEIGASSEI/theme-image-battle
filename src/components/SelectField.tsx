795e344b770ebded4c36837dc045656ae1b2191e
uuronn <uuronprogram@gmail.com>
2023-09-08 01:51:54 +0900

    Create: SelectFieldコンポーネントを作成

---
+++ src/components/SelectField.tsx
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react";
+import { colors } from "~/styles/themes/colors";
+
+type SelectFieldProps = {
+  optionList: string[];
+  placeholder: string;
+};
+
+export const SelectField = ({ optionList, placeholder }: SelectFieldProps) => {
+  return (
+    <select css={select}>
+      <option selected hidden>
+        <span>{placeholder}</span>
+      </option>
+      {optionList.map((option) => (
+        <option>{option}</option>
+      ))}
+    </select>
+  );
+};
+
+const select = css`
+  width: 100%;
+  font-size: 2rem;
+  padding: 8px 12px;
+  border-radius: 8px;
+  background: #ecd55b;
+  box-shadow: inset 5px 5px 5px #d4c052, inset -5px -5px 5px #ffea64;
+  transition: 0.3s;
+  color: ${colors.black.lighten[1]};
+  background: #ecd55b;
+
+  &:hover {
+    box-shadow: inset 2px 2px 3px #d4c052, inset -2px -2px 3px #ffea64;
+  }
+`;
