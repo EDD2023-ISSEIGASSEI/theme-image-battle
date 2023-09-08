7a28afa53c04b5f725c18b9c8e7d4a7762fb02ad
uuronn <uuronprogram@gmail.com>
2023-09-08 16:40:24 +0900

    Fix: SelectFieldを修正

--- src/components/SelectField.tsx
+++ src/components/SelectField.tsx
@@ -9,11 +9,11 @@ type SelectFieldProps = {
 export const SelectField = ({ optionList, placeholder }: SelectFieldProps) => {
   return (
     <select css={select}>
-      <option selected hidden>
-        <span>{placeholder}</span>
+      <option defaultValue={placeholder} hidden>
+        {placeholder}
       </option>
       {optionList.map((option) => (
-        <option>{option}</option>
+        <option key={option}>{option}</option>
       ))}
     </select>
   );
@@ -29,6 +29,7 @@ const select = css`
   transition: 0.3s;
   color: ${colors.black.lighten[1]};
   background: #ecd55b;
+  cursor: pointer;
 
   &:hover {
     box-shadow: inset 2px 2px 3px #d4c052, inset -2px -2px 3px #ffea64;
