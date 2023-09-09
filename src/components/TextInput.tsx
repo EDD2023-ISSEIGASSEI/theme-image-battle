cd1f103ab21b010b9d7591f8ad743e72ee4b5399
uuronn <uuronprogram@gmail.com>
2023-09-09 11:38:44 +0900

    hotFix

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,13 +1,13 @@
 import { css } from "@emotion/react";
 import { colors } from "../styles/themes/colors";
-import React, { ComponentPropsWithRef } from "react";
+import { ComponentPropsWithRef } from "react";
 
 interface TextInputProps extends ComponentPropsWithRef<"input"> {
   placeholder: string;
 }
 
 export const TextInput = ({ placeholder, ...props }: TextInputProps) => {
-  return <input type="text" placeholder={placeholder} css={input} {...props}/>;
+  return <input type="text" placeholder={placeholder} css={input} {...props} />;
 };
 
 const input = css`
