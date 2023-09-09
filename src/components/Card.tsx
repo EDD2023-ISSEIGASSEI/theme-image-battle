eea17994e407b32c1b77bf3a287754a0cc4ba0a4
smaru1111 <marusou20@gmail.com>
2023-09-09 19:33:07 +0900

    #5 update Cardコンポーネントの調整

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,7 +17,7 @@ export const Card = ({ children, ...props }: CardProps) => {
 const card = css`
   font-size: 2rem;
   width: 100%;
-  max-width: 800px;
+  max-width: 700px;
   margin: 0 auto;
   padding: 30px 65px;
   border-radius: 20px;
