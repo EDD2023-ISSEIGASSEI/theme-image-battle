3f794ca0a9c73ecde7fed3b44bc7c82a9b5e852c
shunsuke-tamura <wwwxx4869@gmail.com>
2023-09-30 02:07:01 +0900

    add: Phaseのconst定義

---
+++ src/constant/constant.ts
@@ -0,0 +1,9 @@
+export const PHASE = {
+  waiting: "WaitingPhase",
+  generate: "GeneratePhase",
+  guess: "GuessPhase",
+  showScore: "ShowScorePhase",
+  showCorrectAnswer: "ShowCorrectAnswerPhase",
+  ending: "EndingPhase",
+  closed: "ClosedPhase"
+};
